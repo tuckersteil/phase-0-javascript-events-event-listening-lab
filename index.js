c74408373e76415305870953fe4de097a3946e8b
Tucker Steil <tuckersteil@gmail.com>
2022-04-12 16:34:43 -0400

    commit tree 0d1d28dab6d3fc9501b35778f3fa13c29e854b3d

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', clickAlert);
 }
+function clickAlert() {
+    alert('I was clicked!');
+  }
+
+addingEventListener()
